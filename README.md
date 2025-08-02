389b6c84d2fce26386e350dd3a5b52c92a8a8e5a
i-mein <i.meintanis@gmail.com>
2025-08-02 18:06:29 +0300

    init codebase

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Multi-Agent-Research-Assistant-CrewAI
+# Multi-Agent Research Assistant
 A multi-agent system with CrewAI
+
+
+## Installation
+
+Ensure you have Python >=3.10 <3.13 installed on your system. This project uses [UV](https://docs.astral.sh/uv/) for dependency management and package handling, offering a seamless setup and execution experience.
+
+First, if you haven't already, install uv:
+
+```bash
+pip install uv
+```
+
+Next, navigate to your project directory and install the dependencies:
+
+(Optional) Lock the dependencies and install them by using the CLI command:
+```bash
+crewai install
+```
+### Customizing
+
+**Add your `OPENAI_API_KEY` into the `.env` file**
+
+- Modify `src/researchagent_ollama_gemma3/config/agents.yaml` to define your agents
+- Modify `src/researchagent_ollama_gemma3/config/tasks.yaml` to define your tasks
+- Modify `src/researchagent_ollama_gemma3/crew.py` to add your own logic, tools and specific args
+- Modify `src/researchagent_ollama_gemma3/main.py` to add custom inputs for your agents and tasks
+
+## Running the Project
+
+To kickstart your crew of AI agents and begin task execution, run this from the root folder of your project:
+
+```bash
+$ crewai run
+```
+
+This command initializes the researchagent_ollama_gemma3 Crew, assembling the agents and assigning them tasks as defined in your configuration.
+
+This example, unmodified, will run the create a `report.md` file with the output of a research on LLMs in the root folder.
+
+## Understanding Your Crew
+
+The researchagent_ollama_gemma3 Crew is composed of multiple AI agents, each with unique roles, goals, and tools. These agents collaborate on a series of tasks, defined in `config/tasks.yaml`, leveraging their collective skills to achieve complex objectives. The `config/agents.yaml` file outlines the capabilities and configurations of each agent in your crew.
---
+++ examples-crewai/research_agent/src/researchagent/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import os, sys, gc
+import warnings
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+
+from researchagent_ollama_gemma3.crew import ResearchagentOllamaGemma3
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+import agentops
+
+agentops.init(api_key=os.getenv("AGENTOPS_API_KEY"), auto_start_session=False)
+
+
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs',
+        'current_year': str(datetime.now().year)
+    }
+    
+    try:
+        ResearchagentOllamaGemma3().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        ResearchagentOllamaGemma3().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        ResearchagentOllamaGemma3().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    
+    try:
+        ResearchagentOllamaGemma3().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
